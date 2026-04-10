38d023dde8fa06db46bcc323e721f26884ad19fb
kudala-bharani <bunnykudala@gmail.com>
2026-04-09 22:52:47 -0400

    fixed rendering issues

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
   const isProduction = mode === 'production';
   return {
-    base: isProduction ? '/new-portfolio/' : '/',
+    base: '/',
     plugins: [react()],
     optimizeDeps: {
       exclude: ['lucide-react'],
